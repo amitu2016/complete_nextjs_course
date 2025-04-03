6393414f0ed313efdc0a6721e487bbcb956ced55
Amit Upadhyay <amitu2016@gmail.com>
2025-04-03 06:52:44 +0530

    commit

---
+++ src/app/dashboard/(userGroup)/user/[slug]/page.jsx
@@ -0,0 +1,3 @@
+export default function CustomUser({ params }){
+    return <h1>Welcome {params.slug}</h1>
+}
---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,6 @@
+export default function Dashboard({ children }){
+    return <>
+    <h1>Layout Here</h1>
+    {children}
+    </>
+}
